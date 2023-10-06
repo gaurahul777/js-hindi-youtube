145b6f02bdff8d1ff6bc86e1902e9c1d21154c7a
Rahul Gaur <65395810+gaurahul777@users.noreply.github.com>
2023-10-06 11:06:58 +0000

    data type

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId=144553
+let accountEmail="rahul@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,4 @@
+"use strict"//treat all js code as newer version
+
+//alert(3+3) //we are using nodejs,not browser
+//type of null is object and type of undefined is undefined
