6c0d025fc8486d3d567b3364c9f9071ec767fd60
Rahul Gaur <65395810+gaurahul777@users.noreply.github.com>
2023-10-10 17:04:54 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,17 @@
+const game=new String("rahul");
+// console.log(game);
+
+
+//strings function not change in org value its return another string as a output
+// console.log(game[0]);
+// console.log(game.__proto__.toUpperCase());
+// console.log(game.toUpperCase());
+// console.log(game);
+
+// const gameName=game.slice(-5,5)
+// console.log(gameName);
+//includes yeh hain ya nhi string mr
+// charat
+//replace 
+//split fun split string and gives array based on that you want on which the string got splitexample hitesh-com-chai-
+//split('-') output:-- "hitesh" "com" "chai"
