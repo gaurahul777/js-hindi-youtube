f31a1a36b5bd7a8789ebf259d4ea062ddeaf5380
Rahul Gaur <65395810+gaurahul777@users.noreply.github.com>
2023-10-10 07:00:58 +0000

    heap,stack and const with heap

--- 01_basics/05_datatypes2.js
+++ 01_basics/05_datatypes2.js
@@ -3,10 +3,10 @@
 const id=Symbol('123')
 const id2=Symbol('123')
 
-console.log(id==id2)//fa;se
+// console.log(id==id2)//fa;se
 
 const bigNumber=509095095499499494449448n;
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
 //n lga ker bigint bna do and decimal ko bbhi number bolengye idhr
 
@@ -16,4 +16,27 @@ const myobj={
     name:"rahul",
     age:190,
 }
-console.log(typeof myobj);//object
+// console.log(typeof myobj);//object
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++
+
+// let arr1=["orange","apple"]
+// let arr2=arr1.slice(0)
+// let arr2=[].concat(arr1)
+// let arr2=[...arr1]
+// let arr2=arr1
+// let arr2=["orange","apple"]
+// arr1[0]="mango"
+// console.log(arr1==arr2);
+// console.log(arr1[0]);
+// console.log(arr2[0]);
+
+
+
+let fruits=[1,2,3,4]
+console.log(fruits);
+fruits=[1,2,3,4,5,6,7,8]//not acceptable in const value not change 
+console.log(fruits);
